2515ce16f75811ccc084b260a7fc4a63e73d11ad
Regev Myers <regev.myers@gmail.com>
2026-01-16 19:02:29 +0200

    Sync: Fri Jan 16 19:02:29 IST 2026

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,9 @@
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%=%{(v:lnum==line('.'))?('-> '):(v:lnum)} "
+vim.opt.statuscolumn = "%=%{(v:lnum==line('.'))?('=> '):(v:lnum)} "
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#404040" })
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
 
