73d264cacfe6f8167c5ebe204b307b56416880f7
Regev Myers <regev.myers@gmail.com>
2026-03-03 19:10:39 +0200

    Sync: Tue Mar  3 05:10:39 PM UTC 2026

--- bash/.config/bash/dotfiles.sh
+++ bash/.config/bash/dotfiles.sh
@@ -32,7 +32,7 @@ function dotfiles {
       quiet git add -A
       
       __date="$(date -u)"
-      echo "  |---> Time: $__date"
+      echo "  |---> Commit Message: 'Sync: $__date'"
 
       quiet git commit -m "Sync: $__date" 
       quiet git pull --rebase origin main 
