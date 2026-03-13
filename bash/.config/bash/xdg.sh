d79c976db3f93d9117a7a422b30f48940af8bda6
Regev Myers <regev.myers@gmail.com>
2026-03-13 16:02:01 +0200

    Sync: Fri Mar 13 14:02:01 UTC 2026

--- bash/.config/bash/xdg.sh
+++ bash/.config/bash/xdg.sh
@@ -7,5 +7,6 @@ export HUSHLOGIN_FILE="$XDG_STATE_HOME/bash/hushlogin"
 touch $HUSHLOGIN_FILE
 
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export CARGO_INSTALL_ROOT="$CARGO_HOME"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 
